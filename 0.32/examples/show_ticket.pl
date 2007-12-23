190c69b6901872c3ff5235c9303d93f5cb4b71df
dtikhonov <dtikhonov@a6ca07a9-ea41-0410-884d-899493318fb8>
2007-12-23 22:13:26 +0000

    Relese 0.32

---
+++ 0.32/examples/show_ticket.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+#
+# show_ticket.pl -- retrieve an RT ticket.
+
+use strict;
+use warnings;
+
+use Error qw(:try);
+use RT::Client::REST;
+use RT::Client::REST::Ticket;
+
+unless (@ARGV >= 3) {
+    die "Usage: $0 username password ticket_id\n";
+}
+
+my $rt = RT::Client::REST->new(
+    server  => ($ENV{RTSERVER} || 'http://rt.cpan.org'),
+);
+$rt->login(
+    username=> shift(@ARGV),
+    password=> shift(@ARGV),
+);
+
+RT::Client::REST::Object->use_single_rt($rt);
+RT::Client::REST::Object->use_autoget(1);
+RT::Client::REST::Object->use_autosync(1);
+
+my $ticket;
+try {
+    $ticket = RT::Client::REST::Ticket->new(
+        id  => shift(@ARGV),
+    );
+} catch Exception::Class::Base with {
+    my $e = shift;
+    die ref($e), ": ", $e->message || $e->description, "\n";
+};
+
+use Data::Dumper;
+print Dumper($ticket);
+
+for my $cf (sort $ticket->cf) {
+    print "Custom field '$cf'=", $ticket->cf($cf), "\n";
+}
